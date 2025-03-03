5cfcb22760a231d31e764884f6149ebc2cdd0b9f
LucasGabrielDevs <lucasgabrieldev11@gmail.com>
2025-03-03 17:57:49 -0300

    de hoje

--- academia/src/pages/entrada/index.tsx
+++ academia/src/pages/entrada/index.tsx
@@ -1,30 +1,22 @@
 import React from "react";
 import {Text, View, TextInput, Image} from "react-native"
+import { Style } from "./styles";
+import logo from '../../assets/logo.jpg'
 
 export default function Entrada () {
 
  return (
-    <View>
-
-        <View>
-
-            <View>
-
-
-                <View>
-
-                    <Text>
-
-                        eae parca asda d sdaSA asdad ad
-                    </Text>
-
-                </View>
-
-
-
-            </View>
-
-
+    <View >
+       
+        <View style={Style.topo}>
+                <Text>Seja Bem Vindo</Text>
+                <Image source={logo} />
+        </View>
+        <View style={Style.meio}>
+        <Text>eee</Text>
+        </View>
+        <View style={Style.baixo}>
+        <Text>eee</Text>
         </View>
     
     </View>
---
+++ academia/src/pages/entrada/styles.ts
@@ -0,0 +1,36 @@
+import { StyleSheet, TouchableOpacity } from "react-native";
+
+export const Style = StyleSheet.create({
+
+    topo:{
+        flex:1,
+        alignItems: "center",
+        justifyContent:"center",
+        backgroundColor:'red',
+        width: 80
+    },
+
+    meio:{
+
+        flex:1,
+        alignItems: "center",
+        justifyContent:"center",
+        backgroundColor:'yellow',
+        width:50
+    },
+
+    baixo:{
+
+        flex:1,
+        alignItems: "center",
+        justifyContent:"center",
+         backgroundColor:'blue',
+        width:20
+    
+    }
+
+
+
+
+
+})
